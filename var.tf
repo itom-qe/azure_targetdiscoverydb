264b16bf4d7c7592a61f9bb5011af1ec30dd7726
itom-qe <79703359+itom-qe@users.noreply.github.com>
2023-05-29 21:21:17 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -11,13 +11,21 @@ variable "VMSize" {
   default = "Standard_H8m"
 }
 
-variable "azure_subscription_id" {}
+variable "azure_subscription_id" {
+  default = "061b9311-9c54-4471-9a59-4909517d6f07"
+}
 
-variable "azure_client_id" {}
+variable "azure_client_id" {
+  default = "9d5dac73-c247-4f6e-93b7-c6f0f8f3eb13"
+}
 
-variable "azure_client_secret" {}
+variable "azure_client_secret" {
+  default = "~Zg8Q~8Byz5G0YackT_wxmWJTFxEqr26YWuRdb_C"
+}
 
-variable "azure_tenant_id" {}
+variable "azure_tenant_id" {
+  default = "8bcff170-9979-491e-8683-d8ced0850bad"
+}
 
 variable "azure_rgname" {
   description = "Resource group name"
