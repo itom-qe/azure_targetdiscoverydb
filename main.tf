3be43f898e2f4fe47517f16a11f65a25845c3bfc
itom-qe <79703359+itom-qe@users.noreply.github.com>
2023-05-30 00:23:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,17 +14,6 @@ resource "azurerm_mssql_server" "example" {
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd!"
-  
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
 
 }
 
