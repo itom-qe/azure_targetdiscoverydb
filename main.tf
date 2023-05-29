d0342a3c0eb9b0640f4c74b598203a0a8ab0ff76
itom-qe <79703359+itom-qe@users.noreply.github.com>
2023-05-30 00:37:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,10 +27,19 @@ resource "azurerm_mssql_database" "example" {
   #location                         = "${azurerm_resource_group.rg.location}"
   #server_name                      = "${azurerm_mssql_server.example.name}"
   server_id                         = "${azurerm_mssql_server.example.id}"
-  edition                          = "Basic"
+  #edition                          = "Basic"
   collation                        = "SQL_Latin1_General_CP1_CI_AS"
   create_mode                      = "Default"
   requested_service_objective_name = "Basic"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+
+  tags = {
+    foo = "bar"
+  }
 }
 
 # Enables the "Allow Access to Azure services" box as described in the API docs
