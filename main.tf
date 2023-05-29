f8c6a4508918e8b1abf6dd917423d0e09ff66921
itom-qe <79703359+itom-qe@users.noreply.github.com>
2023-05-30 00:39:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,54 +6,36 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-    name                  = var.azure_rgname
-    location              = var.location
-}
 
-resource "azurerm_mssql_server" "example" {
+resource "azurerm_sql_server" "example" {
   name                         = "${var.prefix}-sqlsvr${random_id.id.hex}"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
-  location                     = "${azurerm_resource_group.rg.location}"
+  resource_group_name          = "${var.azure_rgname}"
+  location                     = "${var.location}"
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd!"
-
 }
 
-resource "azurerm_mssql_database" "example" {
+resource "azurerm_sql_database" "example" {
   name                             = "${var.prefix}-db"
-  #resource_group_name              = "${azurerm_resource_group.rg.name}"
-  #location                         = "${azurerm_resource_group.rg.location}"
-  #server_name                      = "${azurerm_mssql_server.example.name}"
-  server_id                         = "${azurerm_mssql_server.example.id}"
-  #edition                          = "Basic"
+  resource_group_name              = "${var.azure_rgname}"
+  location                         = "${var.location}"
+  server_name                      = "${azurerm_sql_server.example.name}"
+  edition                          = "Basic"
   collation                        = "SQL_Latin1_General_CP1_CI_AS"
   create_mode                      = "Default"
-  #requested_service_objective_name = "Basic"
-  license_type   = "LicenseIncluded"
-  max_size_gb    = 4
-  read_scale     = true
-  sku_name       = "S0"
-  zone_redundant = true
-
-  tags = {
-    foo = "bar"
-  }
+  requested_service_objective_name = "Basic"
 }
 
 # Enables the "Allow Access to Azure services" box as described in the API docs
 # https://docs.microsoft.com/en-us/rest/api/sql/firewallrules/createorupdate
-resource "azurerm_mssql_firewall_rule" "example" {
+resource "azurerm_sql_firewall_rule" "example" {
   name                = "allow-azure-services"
-  #resource_group_name = "${azurerm_resource_group.rg.name}"
-  #server_name         = "${azurerm_mssql_server.example.name}"
-  server_id           = "${azurerm_mssql_server.example.id}"
+  resource_group_name = "${var.azure_rgname}"
+  server_name         = "${azurerm_sql_server.example.name}"
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
-
-
 
 resource "random_id" "id" {
   byte_length = 2
